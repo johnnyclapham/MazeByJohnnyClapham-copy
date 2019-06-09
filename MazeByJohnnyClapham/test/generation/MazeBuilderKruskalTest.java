5a0e3eb87457d9742022aa32277cb22b2325ae11
johnnyclapham <jmclapham@email.wm.edu>
2019-06-09 14:01:30 -0400

    kruskal testing started

--- MazeByJohnnyClapham/test/generation/MazeBuilderKruskalTest.java
+++ MazeByJohnnyClapham/test/generation/MazeBuilderKruskalTest.java
@@ -1,14 +1,32 @@
 package generation;
 
 import static org.junit.Assert.*;
-
+import generation.Order.Builder;
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 
-public class MazeBuilderKruskalTest {
+public class MazeBuilderKruskalTest extends MazeFactoryTest{
+	private MazeFactory mazeFactory;
+	private DummyOrder dummyOrder;
+	private MazeConfiguration configuration;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		boolean det = true;
+		dummyOrder = new DummyOrder(1, Builder.Kruskal, det);
+		mazeFactory.order(dummyOrder);
+		mazeFactory.waitTillDelivered();
+		configuration = dummyOrder.getConfiguration();
 	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+    public void doesKruskalBuild() {
+        assertNotNull(dummyOrder);
+    }
 
 }
