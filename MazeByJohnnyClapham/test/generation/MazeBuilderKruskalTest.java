7aa89f29fabd872312e0617583ee9dd88f3a6fc7
johnnyclapham <jmclapham@email.wm.edu>
2019-06-09 14:23:24 -0400

    Added changeNeighbourValTest()

--- MazeByJohnnyClapham/test/generation/MazeBuilderKruskalTest.java
+++ MazeByJohnnyClapham/test/generation/MazeBuilderKruskalTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
 
-public class MazeBuilderKruskalTest extends MazeFactoryTest{
+public class MazeBuilderKruskalTest{
 	private MazeFactory mazeFactory;
 	private DummyOrder dummyOrder;
 	private MazeConfiguration configuration;
@@ -14,12 +14,12 @@ public class MazeBuilderKruskalTest extends MazeFactoryTest{
 	@Before
 	public void setUp() {
 		boolean det = true;
+		mazeFactory = new MazeFactory(det);
 		dummyOrder = new DummyOrder(1, Builder.Kruskal, det);
 		mazeFactory.order(dummyOrder);
 		mazeFactory.waitTillDelivered();
 		configuration = dummyOrder.getConfiguration();
-	}
-	
+	}	
 	@After
 	public void tearDown() throws Exception {
 	}
@@ -29,4 +29,27 @@ public class MazeBuilderKruskalTest extends MazeFactoryTest{
         assertNotNull(dummyOrder);
     }
 
-}
+	@Test
+	public void changeNeighbourValTest(){
+		int[][] mazeMatrix = new int[5][5];
+		for (int i = 0; i < 5; i++){ // loop through 2d array
+			for (int m = 0; m < 5; m++){
+				
+				mazeMatrix[i][m] = 5; 
+				}
+			}
+		
+		for (int is = 0; is < 5; is++){ // loop through 2d array
+			for (int fm = 0; fm < 5; fm++){
+				
+				assertTrue(mazeMatrix[is][fm] == 5); 
+				}
+			}
+		}
+	
+	
+		
+	}
+	
+	
+
